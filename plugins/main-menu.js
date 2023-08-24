6760c5a25a2908987d626af646500af46d2a4cfe
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-08-24 11:35:18 +0530

    ❍━𝙌𝙐𝙀𝙀𝙉-𝙎𝘼𝙆𝙐𝙍𝘼-𝙈𝘿━❍

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -5,50 +5,53 @@ import fetch from 'node-fetch'
 import { xpRange } from '../lib/levelling.js'
 //import { plugins } from '../lib/plugins.js'
 let tags = {
-  'main': 'ACERCA DE',
-  'game': 'JUEGOS',
-  'econ': 'NIVEL & ECONOMIA',
-  'rg': 'REGISTRO',
-  'sticker': 'STICKER',
-  'img': 'IMAGEN',
-  'maker': 'MAKER',
-  'prem': 'PREMIUM',
-  'group': 'GRUPO',
-  'nable': 'EN/DISABLE OPCIONES', 
-  'nime': 'ANIME',
-  'rnime': 'ANIME REACCION',
-  'dl': 'DESCARGAS',
-  'tools': 'TOOLS',
-  'fun': 'FUN',
-  'cmd': 'DATABASE',
-  'nsfw': 'NSFW +18', 
-  'ansfw': 'NSFW ANIME',
-  'owner': 'OWNER', 
-  'advanced': 'AVANZADO',
+  'main': '👸 *BOT-DETAILS* 👸',
+  'game': '🎮 *GAMES-MENU* 🎮 ',
+  'econ': '💵 *WALLET* 💶',
+  'rg': '🎀 *REGISTOR* 🎀',
+  'sticker': '🪀 *STICKER-MENU* 🪀',
+  'img': '🏷️ *IMAGE-MENU* 🏷️',
+  'maker': '🛠️ *MAKER-MENU* 🛠️',
+  'prem': '🤴 *PREMIUM-MENU* 🤴',
+  'group': '⚡ *GROUP-MENU* ⚡',
+  'nable': '🍭 *EN/DISABLE OPCIONES* 🍭', 
+  'nime': '🔱 *ANIME-MENU* 🔱',
+  'rnime': '🌿 *ANIME-REACCION* 🌿',
+  'dl': '📩 *DOWNLOAD-MENU* 📩',
+  'tools': '🍃 *TOOLS-MENU* 🍃',
+  'fun': '🌀 *FUN-MENU* 🌀',
+  'cmd': '🧬 *DATABASE-MENU* 🧬',
+  'nsfw': '❌ *NSFW +18* ❌', 
+  'ansfw': '❌ *NSFW-ANIME* ❌',
+  'owner': '👤 *OWNER-MENU* 👤', 
+  'advanced': '🧨 *ADD-MENU* 🧨',
 }
 const defaultMenu = {
   before: `
-◈ ━━━━━ *DyLux  ┃ ᴮᴼᵀ* ━━━━━ ◈
- 
-👋🏻 _Hola_ *%name*
-🧿 Nivel : *%level* 
-👥 Usuarios : %totalreg
-📈 Tiempo activo : %muptime
-─────────────
-▢ Crea tu propio bot 
-• https://youtu.be/xFqjKN1Qt80
-▢ Descarga *FGWhatsApp*
-• https://fgmods.epizy.com
-─────────────
-%readmore
-Ⓟ = Premium
-ⓓ = Diamantes
------  -----  -----  -----  -----
-  ≡ *LISTA DE MENUS*
+┏━━━━━━┃👸Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ👸┃━━━━━━❍
+┃┏━━━━━┻⚋⚋⚋⚋⚋⚋♢⚋⚋⚋⚋⚋┻━━━❍
+┃┃👋 *HI* : *%name*
+┃┣━━━━━━━━━━━━❍
+┃┃🎀 *Level*     : *%level* 
+┃┃🔱 *Users*     : *%totalreg*
+┃┃⏳ *Run Time* : *%muptime*
+┃┃👸 *Bot Name* : *QUEEN-SAKURA*
+┃┃👩‍💻 *Creator*    : *Dark-Legend-Hyper*
+┃┃⚡ *Nice To Meet You* *%name* 👸
+┃┃© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ-ᴡᴀ_ʙᴏᴛ
+┃┗━━━━━━━━━━━━━━━━━━━━━━━━━❍
+┗━━━━━━━━━━━━━━━━━━━━━━━❍
+  ┏━━❬ Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ-ᴄᴏᴍᴍᴀɴᴅ-ʟɪꜱᴛ ❭━━┓
+┏▢┓
+┃▢┃ 🅿️ = ᴘʀᴇᴍɪᴜᴍ ᴄᴏᴍᴍᴀɴᴅ 
+┃▢┃ 🆓 = ꜰʀᴇᴇ ᴜꜱᴇ ᴄᴏᴍᴍᴀɴᴅ
+┗▢┛
+⚡━━━━━━❬ᴠɪᴘ ᴡʜᴀᴛꜱᴀᴘᴘ-ʙᴏᴛ❭━━━━━━━⚡
+ ❬━━━━━━❍ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ ❍━━━━━━━❭
 `.trimStart(),
-  header: '┌─⊷ *%category*',
-  body: '▢ %cmd %isdiamond %isPremium',
-  footer: '└───────────\n',
+  header: '╭⚋⚋⚋⚋❬ *%category* ❭⚋⚋⚋❍',
+  body: '┣❍ 🔖 *%cmd* %isPremium',
+  footer: '┗⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋\n',
   after: `
 `,
 }
@@ -113,7 +116,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let header = conn.menu.header || defaultMenu.header
     let body = conn.menu.body || defaultMenu.body
     let footer = conn.menu.footer || defaultMenu.footer
-    let after = conn.menu.after || (conn.user.jid == conn.user.jid ? '' : `Powered by https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let after = conn.menu.after || (conn.user.jid == conn.user.jid ? '' : `*Powered by* https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
     let _text = [
       before,
       ...Object.keys(tags).map(tag => {
@@ -121,8 +124,8 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
           ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
             return menu.help.map(help => {
               return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                .replace(/%isdiamond/g, menu.diamond ? '(ⓓ)' : '')
-                .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '')
+                .replace(/%isdiamond/g, menu.diamond ? '(🆓)' : '')
+                .replace(/%isPremium/g, menu.premium ? '(🅿️)' : '')
                 .trim()
             }).join('\n')
           }),
@@ -158,16 +161,205 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
       ['⌬ Grupos', `${_p}gpdylux`]
     ],m, rpl)*/
   
-    m.react('📚') 
+    m.react('🔠') 
     
   } catch (e) {
-    conn.reply(m.chat, '❎ Lo sentimos, el menú tiene un error', m)
+    conn.reply(m.chat, '❎  Sorry, the menu has an error', m)
     throw e
   }
 }
 handler.help = ['help']
 handler.tags = ['main']
-handler.command = ['menu', 'help', 'menú'] 
+handler.command = ['menu', 'help', 'list'] 
+handler.register = false
+
+handler.exp = 3
+
+export default handler
+
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+function clockString(ms) {
+  let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000)
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [d, 'd ', h, 'h ', m, 'm '].map(v => v.toString().padStart(2, 0)).join('')
+}
+//import db from '../lib/database.js'
+import { promises } from 'fs'
+import { join } from 'path'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+//import { plugins } from '../lib/plugins.js'
+let tags = {
+  'main': '👸 *BOT-DETAILS* 👸',
+  'game': '🎮 *GAMES-MENU* 🎮 ',
+  'econ': '💵 *WALLET* 💶',
+  'rg': '🎀 *REGISTOR* 🎀',
+  'sticker': '🪀 *STICKER-MENU* 🪀',
+  'img': '🏷️ *IMAGE-MENU* 🏷️',
+  'maker': '🛠️ *MAKER-MENU* 🛠️',
+  'prem': '🤴 *PREMIUM-MENU* 🤴',
+  'group': '⚡ *GROUP-MENU* ⚡',
+  'nable': '🍭 *EN/DISABLE OPCIONES* 🍭', 
+  'nime': '🔱 *ANIME-MENU* 🔱',
+  'rnime': '🌿 *ANIME-REACCION* 🌿',
+  'dl': '📩 *DOWNLOAD-MENU* 📩',
+  'tools': '🍃 *TOOLS-MENU* 🍃',
+  'fun': '🌀 *FUN-MENU* 🌀',
+  'cmd': '🧬 *DATABASE-MENU* 🧬',
+  'nsfw': '❌ *NSFW +18* ❌', 
+  'ansfw': '❌ *NSFW-ANIME* ❌',
+  'owner': '👤 *OWNER-MENU* 👤', 
+  'advanced': '🧨 *ADD-MENU* 🧨',
+}
+const defaultMenu = {
+  before: `
+┏━━━━━━┃👸Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ👸┃━━━━━━❍
+┃┏━━━━━┻⚋⚋⚋⚋⚋⚋♢⚋⚋⚋⚋⚋┻━━━❍
+┃┃👋 *HI* : *%name*
+┃┣━━━━━━━━━━━━❍
+┃┃🎀 *Level*     : *%level* 
+┃┃🔱 *Users*     : *%totalreg*
+┃┃⏳ *Run Time* : *%muptime*
+┃┃👸 *Bot Name* : *QUEEN-SAKURA*
+┃┃👩‍💻 *Creator*    : *Dark-Legend-Hyper*
+┃┃⚡ *Nice To Meet You* *%name* 👸
+┃┃© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ-ᴡᴀ_ʙᴏᴛ
+┃┗━━━━━━━━━━━━━━━━━━━━━━━━━❍
+┗━━━━━━━━━━━━━━━━━━━━━━━❍
+  ┏━━❬ Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ-ᴄᴏᴍᴍᴀɴᴅ-ʟɪꜱᴛ ❭━━┓
+┏▢┓
+┃▢┃ 🅿️ = ᴘʀᴇᴍɪᴜᴍ ᴄᴏᴍᴍᴀɴᴅ 
+┃▢┃ 🆓 = ꜰʀᴇᴇ ᴜꜱᴇ ᴄᴏᴍᴍᴀɴᴅ
+┗▢┛
+⚡━━━━━━❬ᴠɪᴘ ᴡʜᴀᴛꜱᴀᴘᴘ-ʙᴏᴛ❭━━━━━━━⚡
+ ❬━━━━━━❍ ᴄᴏᴍᴍᴀɴᴅ ʟɪꜱᴛ ❍━━━━━━━❭
+`.trimStart(),
+  header: '╭⚋⚋⚋⚋❬ *%category* ❭⚋⚋⚋❍',
+  body: '┣❍ 🔖 *%cmd* %isPremium',
+  footer: '┗⚋⚋⚋⚋⚋⚋⚋⚋⚋⚋\n',
+  after: `
+`,
+}
+let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
+  try {
+    let _package = JSON.parse(await promises.readFile(join(__dirname, '../package.json')).catch(_ => ({}))) || {}
+    let { exp, diamond, level, role } = global.db.data.users[m.sender]
+    let { min, xp, max } = xpRange(level, global.multiplier)
+    let name = await conn.getName(m.sender)
+    let d = new Date(new Date + 3600000)
+    let locale = 'es'
+    // d.getTimeZoneOffset()
+    // Offset -420 is 18.00
+    // Offset    0 is  0.00
+    // Offset  420 is  7.00
+    let weton = ['Pahing', 'Pon', 'Wage', 'Kliwon', 'Legi'][Math.floor(d / 84600000) % 5]
+    let week = d.toLocaleDateString(locale, { weekday: 'long' })
+    let date = d.toLocaleDateString(locale, {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    })
+    let dateIslamic = Intl.DateTimeFormat(locale + '-TN-u-ca-islamic', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric'
+    }).format(d)
+    let time = d.toLocaleTimeString(locale, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric'
+    })
+    let _uptime = process.uptime() * 1000
+    let _muptime
+    if (process.send) {
+      process.send('uptime')
+      _muptime = await new Promise(resolve => {
+        process.once('message', resolve)
+        setTimeout(resolve, 1000)
+      }) * 1000
+    }
+    let muptime = clockString(_muptime)
+    let uptime = clockString(_uptime)
+    let totalreg = Object.keys(global.db.data.users).length
+    let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
+    let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+      return {
+        help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+        tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+        prefix: 'customPrefix' in plugin,
+        diamond: plugin.diamond,
+        premium: plugin.premium,
+        enabled: !plugin.disabled,
+      }
+    })
+    for (let plugin of help)
+      if (plugin && 'tags' in plugin)
+        for (let tag of plugin.tags)
+          if (!(tag in tags) && tag) tags[tag] = tag
+    conn.menu = conn.menu ? conn.menu : {}
+    let before = conn.menu.before || defaultMenu.before
+    let header = conn.menu.header || defaultMenu.header
+    let body = conn.menu.body || defaultMenu.body
+    let footer = conn.menu.footer || defaultMenu.footer
+    let after = conn.menu.after || (conn.user.jid == conn.user.jid ? '' : `*Powered by* https://wa.me/${conn.user.jid.split`@`[0]}`) + defaultMenu.after
+    let _text = [
+      before,
+      ...Object.keys(tags).map(tag => {
+        return header.replace(/%category/g, tags[tag]) + '\n' + [
+          ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
+            return menu.help.map(help => {
+              return body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
+                .replace(/%isdiamond/g, menu.diamond ? '(🆓)' : '')
+                .replace(/%isPremium/g, menu.premium ? '(🅿️)' : '')
+                .trim()
+            }).join('\n')
+          }),
+          footer
+        ].join('\n')
+      }),
+      after
+    ].join('\n')
+    let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
+    let replace = {
+      '%': '%',
+      p: _p, uptime, muptime,
+      me: conn.getName(conn.user.jid),
+      npmname: _package.name,
+      npmdesc: _package.description,
+      version: _package.version,
+      exp: exp - min,
+      maxexp: xp,
+      totalexp: exp,
+      xp4levelup: max - exp,
+      github: _package.homepage ? _package.homepage.url || _package.homepage : '[unknown github url]',
+      level, diamond, name, weton, week, date, dateIslamic, time, totalreg, rtotalreg, role,
+      readmore: readMore
+    }
+    text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
+    
+    let pp = './src/fg_logo.jpg'
+     
+    conn.sendFile(m.chat, pp, 'menu.jpg', text.trim(), m, null, rpl)
+    /*conn.sendButton(m.chat, text.trim(), '▢ DyLux  ┃ ᴮᴼᵀ\n▢ Sígueme en Instagram\nhttps://www.instagram.com/fg98_ff', pp, [
+      ['ꨄ︎ Apoyar', `${_p}donate`],
+      ['⏍ Info', `${_p}botinfo`],
+      ['⌬ Grupos', `${_p}gpdylux`]
+    ],m, rpl)*/
+  
+    m.react('🔠') 
+    
+  } catch (e) {
+    conn.reply(m.chat, '❎  Sorry, the menu has an error', m)
+    throw e
+  }
+}
+handler.help = ['help']
+handler.tags = ['main']
+handler.command = ['menu', 'help', 'list'] 
 handler.register = false
 
 handler.exp = 3
