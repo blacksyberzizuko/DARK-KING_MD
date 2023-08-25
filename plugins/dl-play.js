fb3c3fb2ae828b787203ed15d072c3198cc56bc6
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-08-25 06:19:36 +0530

    ❍━𝙌𝙐𝙀𝙀𝙉-𝙎𝘼𝙆𝙐𝙍𝘼-𝙈𝘿━❍

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -18,8 +18,8 @@ let handler = async (m, { conn, command, text, usedPrefix }) => {
 ┣❍ 👀 *views:* ${views}
 └──────────────❍`
  await conn.sendButton(m.chat, play, fgig, thumbnail, [
-    ['🎶 MP3', `${usedPrefix}fgmp3 ${url}`],
-    ['🎥 MP4', `${usedPrefix}fgmp4 ${url}`]
+    ['🎶 MP3', `${usedPrefix}ytmp3 ${url}`],
+    ['🎥 MP4', `${usedPrefix}ytmp4 ${url}`]
   ], m, rpl)
 }
 handler.help = ['play']
