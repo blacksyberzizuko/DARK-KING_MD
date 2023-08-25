df0d0a84aa8ae6b54b6717690270f713fec41cd8
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-08-25 15:38:37 +0530

    ❍━𝙌𝙐𝙀𝙀𝙉-𝙎𝘼𝙆𝙐𝙍𝘼-𝙈𝘿━❍

--- plugins/gp-setwelcome.js
+++ plugins/gp-setwelcome.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, text, isROwner, isOwner }) => {
   if (text) {
     global.db.data.chats[m.chat].sWelcome = text
     m.reply('✅ Se configuro el mensaje de bienvenida')
-  } else throw `✳️ Ingrese el mensaje de Bienvenida\n\n@user (mención)\n@group (Nombre de grupo)\n@desc (description de grupo)`
+  } else throw `┏⚋⚋⚋⚋❬ *Welcome* ❭⚋⚋⚋❍\n\n Hi @user (mención)\n@group (Nombre de grupo)\n@desc (description de grupo)`
 }
 handler.help = ['setwelcome <text>']
 handler.tags = ['group']
