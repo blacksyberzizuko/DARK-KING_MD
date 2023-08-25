dae2773d5b16ca9ec4cf0fcb466e7658b9152d4f
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-08-25 15:29:48 +0530

    ❍━𝙌𝙐𝙀𝙀𝙉-𝙎𝘼𝙆𝙐𝙍𝘼-𝙈𝘿━❍

--- plugins/rg-register.js
+++ plugins/rg-register.js
@@ -7,22 +7,22 @@ let handler = async function (m, { conn, text, usedPrefix, command }) {
   if (user.registered === true) throw `✳️ Ya estás registrado\n\n¿Quiere volver a registrarse?\n\n 📌 Use este comando para eliminar su registro \n*${usedPrefix}unreg* <Número de serie>`
   if (!Reg.test(text)) throw `⚠️ Formato incorrecto\n\n ✳️ Uso del comamdo: *${usedPrefix + command} nombre.edad*\n📌Ejemplo : *${usedPrefix + command}* ${name2}.16`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw '✳️ El nombre no puede estar vacío'
-  if (!age) throw '✳️ La edad no puede estar vacía'
-  if (name.length >= 30) throw '✳️ El nombre es demasiado largo' 
+  if (!name) throw '👸 WHATS YOU R NAME'
+  if (!age) throw '👸 HOW OLD ARE YOU'
+  if (name.length >= 30) throw '👸 LARGE NUMBER ' 
   age = parseInt(age)
-  if (age > 100) throw '👴🏻 Wow el abuelo quiere jugar al bot'
-  if (age < 5) throw '🚼  hay un abuelo bebé jsjsj '
+  if (age > 100) throw '👴🏻Wow you can register '
+  if (age < 5) throw '🚼  YOU CAN T REGISTER  '
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-┌─「 *REGISTRADO* 」─
-▢ *Nombre:* ${name}
-▢ *Edad* : ${age} años
-▢ *Numero de serie* :
+┏⚋⚋⚋❬ *REGISTER* ❭⚋⚋❍
+🔖 *NAME :* ${name}
+🔖 *AGE* : ${age} años
+🔖 *NUMBER SN* :
 ${sn}
 └──────────────
 
