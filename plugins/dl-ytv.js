b07e6be3dddc941683f70559f059a3120235c7be
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-08-25 06:26:22 +0530

    Update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -3,7 +3,7 @@ import fg from 'api-dylux'
 import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
 let limit = 350 
 let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
-	if (!args || !args[0]) throw `✳️ Ejemplo :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
+	if (!args || !args[0]) throw `🔖 Example :\n${usedPrefix + command} https://youtu.be/YzkTFFwxtXI`
     if (!args[0].match(/youtu/gi)) throw `❎ Verifica que el link de YouTube`
 	 let chat = global.db.data.chats[m.chat]
 	 m.react(rwait) 
@@ -17,23 +17,23 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
 		
        if (size.split('MB')[0] >= limit) return m.reply(` ≡  *FG YTDL*\n\n▢ *⚖️Peso* : ${size}\n▢ *🎞️Calidad* : ${q}\n\n▢ _El archivo supera el límite de descarga_ *+${limit} MB*`)    
 	  conn.sendFile(m.chat, dl_url, title + '.mp4', `
- ≡  *FG YTDL*
+ 👸 *Sakura-Yt* 👸
   
-▢ *📌Título* : ${title}
-▢ *📟 Ext* : mp4
-▢ *🎞️Calidad* : ${q}
-▢ *⚖️Peso* : ${size}
+❍ *🎀Tile* : ${title}
+❍ *📟 Ext* : mp4
+❍ *🎞️Calidad* : ${q}
+❍ *⚖️Size* : ${size}
 `.trim(), m, false, { asDocument: chat.useDocument })
 		m.react(done) 
 		
 	} catch {	
-       m.reply(`✳️ Error al descargar el video intenta con otro`) 
+       m.reply(`✳️ Error.....`) 
 	} 
 		 
 }
 handler.help = ['ytmp4 <link yt>']
 handler.tags = ['dl'] 
-handler.command = ['ytmp4', 'fgmp4']
+handler.command = ['ytmp4', 'ytmp4']
 handler.diamond = true
 
 export default handler
