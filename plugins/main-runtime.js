f001151e2a03c6a1c587cd1768873eccaeb34d66
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-08-25 15:32:46 +0530

    ❍━𝙌𝙐𝙀𝙀𝙉-𝙎𝘼𝙆𝙐𝙍𝘼-𝙈𝘿━❍

--- plugins/main-runtime.js
+++ plugins/main-runtime.js
@@ -10,7 +10,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
       }) * 1000
     }
     let muptime = clockString(_muptime)
-   m.reply(`🏮 *Bot activo durante*  \n\n${muptime}`) 
+   m.reply(`🕧  *👸 QUEEN-SAKURA_RUNTIME*  \n\n${muptime}`) 
 }
 handler.help = ['runtime']
 handler.tags = ['main']
