f3f88f0d527fb6dac860ca75aa660ae9e923c57c
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-14 19:40:24 +0530

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,57 +1,292 @@
+import fetch from "node-fetch"
+import ytdl from "ytdl-core"
+import yts from "yt-search"
+import {
+    generateWAMessageFromContent
+} from "@whiskeysockets/baileys"
 
-import yts from 'yt-search'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-let limit = 320
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  
-    if (!text) throw `✳️ Enter a song title\n\n📌 Example *${usedPrefix + command}* *Baby Dont go Baby*`
-  let chat = global.db.data.chats[m.chat]
-  let res = await yts(text)
-  //let vid = res.all.find(video => video.seconds < 3600)
-  let vid = res.videos[0]
-  if (!vid) throw `✳️ Vídeo/Audio no found`
-  let isVideo = /vid$/.test(command)
-  m.react('🎧') 
-  
-  try {
-  let q = isVideo ? '360p' : '128kbps' 
-  let v = vid.url
-  let yt = await youtubedl(v).catch(async () => await youtubedlv2(v))
-  let dl_url = await (isVideo ? yt.video[q].download() : yt.audio[q].download())
-  let title = await yt.title
-  let size = await (isVideo ? yt.video[q].fileSizeH : yt.audio[q].fileSizeH)
-  let play = `
-┏━━━❍ *YT-MUSIC* ━━━❍
-┌──────────────
-┣❍ 📌 *Tile* : ${vid.title}
-┣❍ 📆 *Relise:* ${vid.ago}
-┣❍ ⌚ *Duration:* ${vid.timestamp}
-┣❍ 👀 *Views:* ${vid.views}
-┣──────────────❍
-\n┃©ʙʏ Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ
-┗━━━━━━━━━━━━━━❍
-*Downloading And Uploading Song*`
-conn.sendFile(m.chat, vid.thumbnail, 'song', play, m, null, rpig)
+let limit = 100
+let handler = async (m, {
+    conn,
+    command,
+    text,
+    args,
+    usedPrefix
+}) => {
+    if (!text) throw `Use example *${usedPrefix + command}* DONT GO BABY DONT GO...`
+    const combinedRegex = /^(play|song)$/i;
+    const isMP3 = combinedRegex.test(command);
 
-if (size.split('MB')[0] >= limit) return m.reply(`*YT-DL*\n\n▢ *⚖️Size* : ${size}\n▢ *🎞️Calidad* : ${q}\n\n▢ _The file exceeds the download limit_ *+${limit} MB*`) 
-if (size.includes('GB')) return m.reply(`*YT-DL*\n\n▢ *⚖️Size* : ${size}\n▢ *🎞️Calidad* : ${q}\n\n▢ _File exceeds download limit_ *+${limit} MB*`)   
-	  conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /vid$/.test(command)), `
-┏━━━❍ *Sakura-Yt* ❍━━━
-┣❍ 
-┣❍ *🎀Tile* : ${title}
-┣❍ *🎞️Calidad* : ${q}
-┣❍ *⚖️Size* : ${size}
-┣❍ ©ʙʏ Qᴜᴇᴇɴ-ꜱᴀᴋᴜʀᴀ
-┗━━━━━━━❍
-`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
-		m.react(done) 
-    } catch {
-		m.reply(`Error: 🥺`)
+    try {
+        let vid = await searchAndFilterVideos(text)
+        if (!vid) throw "Video Not Found, Try Another Title"
+        let {
+            title,
+            thumbnail,
+            timestamp,
+            views,
+            ago,
+            url
+        } = vid
+        let dla = "🧚‍♀️QUEEN-SAKURA-MD Downloading audio please wait"
+        let dls = "🧚‍♀️ QUEEN-SAKURA-MD Downloading audio succes"
+
+        let captvid = `*☆: .｡. o *YOUTUBE-DL *o .｡.:☆*
+*⏰Title:* ${title ? title : 'not know'}
+*⌛Duration:* ${timestamp ? timestamp : 'not know'}
+*👀Views:* ${formatNumber(views) ? formatNumber(views) : 'not know'}
+*📅Upload:* ${ago ? ago : 'not know'}
+*🔗Link:* ${url}
+*🧚‍♀️QUEEN-SAKURA-MD...*
+*📤 ඔබගේ ගීතය ලබාදෙමින් පවතී....*`
+        let ytthumb = await (await conn.getFile(thumbnail)).data
+        let msg = await generateWAMessageFromContent(m.chat, {
+            extendedTextMessage: {
+                text: captvid,
+                jpegThumbnail: ytthumb,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    externalAdReply: {
+                        body: dla,
+                        containsAutoReply: true,
+                        mediaType: 1,
+                        mediaUrl: url,
+                        renderLargerThumbnail: true,
+                        showAdAttribution: true,
+                        sourceId: "lua ser ofc",
+                        sourceType: "PDF",
+                        previewType: "PDF",
+                        sourceUrl: url,
+                        thumbnail: ytthumb,
+                        thumbnailUrl: thumbnail,
+                        title: "*🧚‍♀️QUEEN-SAKURA-MD_2023🧚‍♀️*"
+                    }
+                }
+            }
+        }, {
+            quoted: m
+        })
+        await conn.relayMessage(m.chat, msg.message, {})
+
+        if (isMP3) {
+            let Ytdl = await ytmp3(url)
+            let dls = "Play audio succes"
+            let ytthumb = await (await conn.getFile(Ytdl.meta.image)).data
+            let doc = {
+                audio: Ytdl.buffer,
+                mimetype: "audio/mp4",
+                fileName: Ytdl.meta.title,
+                contextInfo: {
+                    externalAdReply: {
+                        showAdAttribution: true,
+                        mediaType: 2,
+                        mediaUrl: url,
+                        title: Ytdl.meta.title,
+                        body: dls,
+                        sourceUrl: url,
+                        thumbnail: ytthumb
+                    }
+                }
+            }
+            await conn.sendMessage(m.chat, doc, {
+                quoted: m
+            })
+        } else {
+            let q = args[1] || "360p"
+            let item = await ytmp4(url, q.split("p")[0])
+            if ((item.contentLength).split("MB")[0] >= limit) return m.reply(` ≡  *YT Downloader*\n\n*⚖️Size* : ${item.contentLength}\n*🎞️Quality* : ${item.quality}\n\n_The file exceeds the download limit_ *+${limit} MB*\n\n*Link:*\n${await shortUrl(item.videoUrl)}`)
+            let captvid = `🔍 *[ RESULT ]*
+
+📷 *Image URL:* ${item.thumb.url || 'not know'}
+📚 *Title:* ${item.title || 'not know'}
+📅 *Date:* ${item.date || 'not know'}
+⏱️ *Duration:* ${item.duration || 'not know'}
+📺 *Channel:* ${item.channel || 'not know'}
+🔒 *Quality:* ${item.quality || 'not know'}
+📦 *Content Length:* ${item.contentLength || 'not know'}
+📝 *Description:* ${item.description || 'not know'}
+`.trim()
+            let dls = "Play video succes"
+            let doc = {
+                video: {
+                    url: item.videoUrl
+                },
+                mimetype: "video/mp4",
+                caption: captvid,
+                contextInfo: {
+                    externalAdReply: {
+                        showAdAttribution: true,
+                        mediaType: 2,
+                        mediaUrl: url,
+                        title: item.title,
+                        body: dls,
+                        sourceUrl: url,
+                        thumbnail: await (await conn.getFile(item.image)).data
+                    }
+                }
+            }
+            await conn.sendMessage(m.chat, doc, {
+                quoted: m
+            })
+        }
+    } catch (e) {
+        await m.reply(eror)
     }
-
 }
 handler.help = ['play']
 handler.tags = ['dl']
 handler.command = ['play', 'song']
 
 export default handler
+
+function formatNumber(num) {
+    const suffixes = ['', 'k', 'M', 'B', 'T'];
+    const numString = Math.abs(num).toString();
+    const numDigits = numString.length;
+
+    if (numDigits <= 3) {
+        return numString;
+    }
+
+    const suffixIndex = Math.floor((numDigits - 1) / 3);
+    let formattedNum = (num / Math.pow(1000, suffixIndex)).toFixed(1);
+
+    // Menghapus desimal jika angka sudah bulat
+    if (formattedNum.endsWith('.0')) {
+        formattedNum = formattedNum.slice(0, -2);
+    }
+
+    return formattedNum + suffixes[suffixIndex];
+}
+
+async function searchAndFilterVideos(query, maxResults = 100, similarityThreshold = 0.5) {
+    try {
+        const res = await yts(query);
+        const videos = res.videos
+            .slice(0, maxResults)
+            .filter(video => {
+                const titleWords = video.title.toLowerCase().split(" ");
+                const queryWords = query.toLowerCase().split(" ");
+                const matchCount = titleWords.filter(word => queryWords.includes(word)).length;
+                return matchCount / titleWords.length >= similarityThreshold;
+            });
+
+        if (videos.length > 0) {
+            return videos[0];
+        } else if (res.videos.length > 0) {
+            return res.videos[0];
+        } else {
+            return {};
+        }
+    } catch (e) {
+        console.error(e);
+        return {};
+    }
+}
+
+async function ytmp3(url) {
+    try {
+        const {
+            videoDetails
+        } = await ytdl.getInfo(url, {
+            lang: "id"
+        });
+
+        const stream = ytdl(url, {
+            filter: "audioonly",
+            quality: 140
+        });
+        const chunks = [];
+
+        stream.on("data", (chunk) => {
+            chunks.push(chunk);
+        });
+
+        await new Promise((resolve, reject) => {
+            stream.on("end", resolve);
+            stream.on("error", reject);
+        });
+
+        const buffer = Buffer.concat(chunks);
+
+        return {
+            meta: {
+                title: videoDetails.title,
+                channel: videoDetails.author.name,
+                seconds: videoDetails.lengthSeconds,
+                description: videoDetails.description,
+                image: videoDetails.thumbnails.slice(-1)[0].url,
+            },
+            buffer: buffer,
+            size: buffer.length,
+        };
+    } catch (error) {
+        throw error;
+    }
+};
+
+async function shortUrl(url) {
+    let res = await fetch(`https://tinyurl.com/api-create.php?url=${url}`)
+    return await res.text()
+}
+
+function formatDuration(seconds) {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    const formattedDuration = [];
+
+    if (hours > 0) {
+        formattedDuration.push(`${hours} hour`);
+    }
+
+    if (minutes > 0) {
+        formattedDuration.push(`${minutes} minute`);
+    }
+
+    if (remainingSeconds > 0) {
+        formattedDuration.push(`${remainingSeconds} second`);
+    }
+
+    return formattedDuration.join(' ');
+}
+
+function formatBytes(bytes) {
+    if (bytes === 0) {
+        return '0 B';
+    }
+    const sizes = ['B', 'KB', 'MB', 'GB', 'TB'];
+    const i = Math.floor(Math.log(bytes) / Math.log(1024));
+    return `${(bytes / 1024 ** i).toFixed(2)} ${sizes[i]}`;
+}
+
+async function ytmp4(query, quality = 134) {
+    try {
+        const videoInfo = await ytdl.getInfo(query, {
+            lang: 'id'
+        });
+        const format = ytdl.chooseFormat(videoInfo.formats, {
+            format: quality,
+            filter: 'videoandaudio'
+        })
+        let response = await fetch(format.url, {
+            method: 'HEAD'
+        });
+        let contentLength = response.headers.get('content-length');
+        let fileSizeInBytes = parseInt(contentLength);
+        return {
+            title: videoInfo.videoDetails.title,
+            thumb: videoInfo.videoDetails.thumbnails.slice(-1)[0],
+            date: videoInfo.videoDetails.publishDate,
+            duration: formatDuration(videoInfo.videoDetails.lengthSeconds),
+            channel: videoInfo.videoDetails.ownerChannelName,
+            quality: format.qualityLabel,
+            contentLength: formatBytes(fileSizeInBytes),
+            description: videoInfo.videoDetails.description,
+            videoUrl: format.url
+        }
+    } catch (error) {
+        throw error
+    }
+}
