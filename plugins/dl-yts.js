41677dce7daab0c416969aab6ffbade0b021ecbf
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-14 19:45:02 +0530

    Update dl-yts.js

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -1,27 +1,19 @@
-
 import yts from 'yt-search'
+import fs from 'fs'
 
 let handler = async (m, {conn, text }) => {
-  if (!text) throw '✳️ Que quieres que busque en YouTube?'
+  if (!text) throw 'What are you looking for?'
+  await conn.reply(m.chat, global.wait, m)
   let results = await yts(text)
   let tes = results.all
   let teks = results.all.map(v => {
     switch (v.type) {
       case 'video': return `
-▢ ${v.title}
-▢ *Link* : ${v.url}
-▢ *Duración* : ${v.timestamp}
-▢ *Subido :* ${v.ago}
-▢ *Vistas:* ${v.views}
-
-   `.trim()
-      case 'canal': return `
-▢ *${v.name}* (${v.url})
-▢${v.subCountLabel} (${v.subCount}) Suscribirse
-▢ ${v.videoCount} videos
-`.trim()
-    }
-  }).filter(v => v).join('\n\n________________________\n\n')
+° *_${v.title}_*
+↳ 🫐 *_Link :_* ${v.url}
+↳ 🕒 *_Duration :_* ${v.timestamp}
+↳ 📥 *_Uploaded :_* ${v.ago}
+↳ 👁 *_Views :_* ${v.views}`}}).filter(v => v).join('\n\n◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦◦\n\n')
   conn.sendFile(m.chat, tes[0].thumbnail, 'yts.jpeg', teks, m)
 }
 handler.help = ['ytsearch'] 
