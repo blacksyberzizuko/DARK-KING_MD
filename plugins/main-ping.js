4d9f01bb3529c8450bd371b323f3a4de6cb4bfc9
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-26 19:09:54 +0530

    Update main-ping.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -8,9 +8,9 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.react('🚀')
           let pp = './src/sakura_dlmenu.jpg' 
-    conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rpl)
+          conn.sendFile(m.chat, pp, 'menu.jpg', m2, m, null, rpl)
+          m.react('🚀')
           m.reply(`${ssd}👸 *PING_BY-SAKURA* : ${latensi.toFixed(4)} _ms_`);
             });
 }
