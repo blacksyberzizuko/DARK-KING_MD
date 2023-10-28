530bd2a2d25a8311a1f0cb9a791c1295310533c4
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-28 11:18:28 +0530

    Update nsfw-xnxxdl.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/nsfw-xnxxdl.js
+++ plugins/nsfw-xnxxdl.js
@@ -4,26 +4,29 @@ import fg from 'api-dylux'
 let handler = async (m, { conn, args, text, usedPrefix, command }) => {
 
  let chat = global.db.data.chats[m.chat]
-  if (!chat.nsfw) throw `🚫 El grupo no admite contenido nsfw \n\nPara habilitar escriba \n*${usedPrefix}enable* nsfw`
+  if (!chat.nsfw) throw `🚫 please *${usedPrefix}enable* nsfw`
   let user = global.db.data.users[m.sender].age
-  if (user < 17) throw `❎ Eres menor de edad! vuelve cuando tengas más de 18 años`
-  if (!text) throw `✳️ Para buscar\n📌 Use : *${usedPrefix + command} <search>*\n\nPara descargar desde URL:\n📌Use : *${usedPrefix + command} <url>*`
+  if (user < 17) throw `❎🔞🔞🔞🔞🔞🔞🔞`
+  if (!text) throw `please enter the url\n📌 Use : *${usedPrefix + command} <search>*:\n📌Use : *${usedPrefix + command} <url>*`
     
-    m.react(rwait)
+    m.react('🔞')
     if (text.includes('http://') || text.includes('https://')) {
-        if (!text.includes('xnxx.com')) return m.reply(`❎ Ingrese un link de *xnxx.com*`)
+        if (!text.includes('xnxx.com')) return m.reply(`❎ enter the link de *xnxx.com*`)
         try {
             let xn = await fg.xnxxdl(text)
             conn.sendFile(m.chat, xn.result.files.high, xn.result.title + '.mp4', `
-≡  *XNXX DL*
-            
-▢ *📌Título*: ${xn.result.title}
-▢ *⌚Duración:* ${xn.result.duration}
-▢ *🎞️Calidad:* ${xn.result.quality}
-`.trim(), m, false, { asDocument: chat.useDocument })
+✘『 𝗦𝗔𝗞𝗨𝗥𝗔-𝗫𝗡𝗫𝗫_𝗗𝗟 』✘
+
+*VIDEO TITLE*         
+📝 *${xn.result.title}*
+*VIDEO DURATION*
+🕗 *${xn.result.duration}*
+*VIDEO QUALITY*
+🎥 *${xn.result.quality}*
+`.trim(), m, false, { asvideo: chat.usevideo })
  m.react(done)
  } catch (e) {
-    m.reply(`🔴 Error : intenta mas tarde`)
+    m.reply(`🔴 Error : ♻️♻️♻️`)
  }
     } else {
         try {
@@ -31,15 +34,15 @@ let handler = async (m, { conn, args, text, usedPrefix, command }) => {
             let ff = res.result.map((v, i) => `${i + 1}┃ *Titulo* : ${v.title}\n*Link:* ${v.link}\n`).join('\n') 
               if (res.status) m.reply(ff)
             } catch (e) {
-              m.reply(`🔴 Error: intenta mas tarde`)
+              m.reply(`🔴 Error:♻️♻️♻️♻️`)
                }
     }
 }
 handler.help = ['xnxx'] 
 handler.tags = ['nsfw', 'prem']
 handler.command = ['xnxxsearch', 'xnxxdl', 'xnxx'] 
-handler.diamond = 2
+handler.diamond = 10
 handler.premium = false
-handler.register = true
+handler.register = false
 
 export default handler
