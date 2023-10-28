f6921c4804dbe11e16afaf929916c49dec13afd7
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-28 07:10:10 +0530

    Update av-audios.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -1,4 +1,4 @@
-
+☀️
 let handler = m => m
 handler.all = async function (m) {
   for (const message in audioMsg) {
@@ -13,13 +13,7 @@ handler.all = async function (m) {
 export default handler
 
 
-let audioMsg = {
-  'fino señores': './src/mp3/fino.mp3',
-  'buenos días': 'https://j.top4top.io/m_26464yyei1.mp3',
-  'buenas tardes': 'https://i.top4top.io/m_2646qxac91.mp3',
-  'buenas noches': 'https://h.top4top.io/m_26460eg6v1.mp3',
-  'sad': 'https://h.top4top.io/m_2474fhcbh1.mp3',
-  '@5492622271736|@59172945992': 'https://l.top4top.io/m_2492i4mdu1.mp3'
+let audioMsg = {'
   '.menu': './SAKURA-MD/Audio-Uploader/.menu.mp3',
   '.alive': './SAKURA-MD/Audio-Uploader/.alive.mp3',
 }
