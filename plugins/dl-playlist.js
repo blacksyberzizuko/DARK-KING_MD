32ca2eeb413577eacfdc180666eeadcb12b1daca
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-28 10:33:07 +0530

    Update dl-playlist.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/dl-playlist.js
+++ plugins/dl-playlist.js
@@ -3,22 +3,22 @@ import { youtubeSearch } from '@bochilteam/scraper'
 import yts from 'yt-search'
 let handler = async(m, { conn, usedPrefix, text, args, command }) => {
 
-    if (!text) throw `✳️ Ingresa el título de una canción\n\n*📌 Ejemplo*\n*${usedPrefix + command}* Lil Peep hate my fuccn life `
+    if (!text) throw `🌀 please Enter The Text \n\n*📌 Example*\n*${usedPrefix + command}* Chilhood `
     m.react('📀')
     let result = await yts(text)
     let ytres = result.videos
     let listSections = []
 	Object.values(ytres).map((v, index) => {
 	listSections.push([`${index}┃ ${v.title}`, [
-          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`],
-          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `▢ ⌚ *Duración:* ${v.timestamp}\n▢ 👀 *Vistas:* ${v.views}\n▢ 📌 *Título* : ${v.title}\n▢ 📆 *Publicado:* ${v.ago}\n`]
+          ['🎶 MP3', `${usedPrefix}fgmp3 ${v.url}`, `⦁ ⏰ *Duration:* ${v.timestamp}\n⦁ 👀 *Views:* ${v.views}\n⦁ 📝 *Title* : ${v.title}\n⦁ 📆 *published day:* ${v.ago}\n`],
+          ['🎥 MP4', `${usedPrefix}fgmp4 ${v.url}`, `⦁ ⏰ *Duration:* ${v.timestamp}\n⦁ 👀 *Views:* ${v.views}\n⦁ 📝 *Title* : ${v.title}\n⦁ 📆 *published day:* ${v.ago}\n`]
         ]])
 	})
-	return conn.sendList(m.chat, '  ≡ *FG MUSIC*🔎', `\n 📀 Aqui una lista de resultados de :\n *${text}*`, fgig, `Click Aquí `, listSections, m)
+	return conn.sendList(m.chat, '✘『𝗦𝗔𝗞𝗨𝗥𝗔-𝗣𝗟𝗔𝗬_𝗟𝗜𝗦𝗧』✘', `\n 📀 PLAYLIST DESCRIPTION  :\n *${text}*`, fgig, `Click Aquí `, listSections, m)
 }
 handler.help = ['play2']
 handler.tags = ['dl']
 handler.command = ['play2', 'playvid2', 'playlist', 'playlista'] 
-handler.disabled = true
+handler.disabled = false
 
 export default handler
