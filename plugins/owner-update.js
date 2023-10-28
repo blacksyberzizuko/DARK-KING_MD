c6f4a912d3e8f09b0a5cf21c7175ea9cf416fc01
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-28 11:07:13 +0530

    Update owner-update.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -3,6 +3,7 @@
 import { execSync } from 'child_process'
 let handler = async (m, { conn, text }) => {
 if (conn.user.jid == conn.user.jid) {
+m.react('♻️')
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
 //require('fs').readdirSync('plugins').map(v=>global.reload('', v))
 conn.reply(m.chat, stdout.toString(), m)
