16be9ed883df33988c07b212da0625ed188fe465
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-10-28 10:49:20 +0530

    Update dl-twitter.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/dl-twitter.js
+++ plugins/dl-twitter.js
@@ -1,24 +1,24 @@
 
 import fg from 'api-dylux'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `📌 Ejemplo : \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
-          m.react(rwait)    
+if (!args[0]) throw `🌀 Example : \n*${usedPrefix + command}* https://twitter.com/fernandavasro/status/1569741835555291139?t=ADxk8P3Z3prq8USIZUqXCg&s=19`
+          m.react('🕗')    
           try {
           let { SD, HD, desc, thumb, audio } = await fg.twitter(args[0])
           let te = ` 
-┌─⊷ *TWITTER DL*
-▢ Descripción: ${desc}
-└───────────`
+✘『𝗦𝗔𝗞𝗨𝗥𝗔-𝗧𝗪𝗜𝗧𝗧𝗘𝗥_𝗗𝗟』✘
+⦁ *Description*: ${desc}
+*🧚‍♂️QUEEN-SAKURA_MD*`
 conn.sendFile(m.chat, HD, 'twitter.mp4', te, m)
 m.react(done)
 } catch (e) {
-  	m.reply(`✳️ verifica que el link sea de Twitter`)
+  	m.reply(`🌀Please enter the correct Url`)
 	} 
 	
 }
 handler.help = ['twitter'].map(v => v + ' <url>')
 handler.tags = ['dl']
 handler.command = /^(twitter|tw)$/i
-handler.diamond = true
+handler.diamond = false
 
 export default handler
