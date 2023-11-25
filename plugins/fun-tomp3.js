5d2bc17b509b3f2daf4aebfcc40b0acfcb873450
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-11-25 19:11:24 +0530

    Update fun-tomp3.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/fun-tomp3.js
+++ plugins/fun-tomp3.js
@@ -4,14 +4,14 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     try {
     let q = m.quoted ? m.quoted : m
    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-   // if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`
+   // if (!/video|audio/.test(mime)) throw `✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`
     let media = await q.download?.()
-    if (!media) throw '❎ Error al descargar medios'
+    if (!media) throw '❎ ERROR THIS MEDIA '
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error al convertir'
+    if (!audio.data) throw '❎ Error  converte'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
     } catch (e) {
-        m.reply(`✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`)
+        m.reply(`✳️ Reply to the video or voice note you want to convert to mp3 with the command :\n\n*${usedPrefix + command}*`)
    }
 }
 handler.help = ['tomp3']
