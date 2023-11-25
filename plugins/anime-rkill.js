9da8ffe3d0787e1e33f5872bdf2708d54e441b22
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-11-25 18:33:52 +0530

    Update anime-rkill.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/anime-rkill.js
+++ plugins/anime-rkill.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 	 let who
     if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
     else who = m.chat
-    if (!who) throw `✳️ Etiqueta o menciona a alguien\n\n📌 Ejemplo : ${usedPrefix + command} @tag` 
+    if (!who) throw `*♻Tag or mention someone*\n\nEXAMPLE : ${usedPrefix + command} @tag` 
     
     let user = global.db.data.users[who]
     let name = conn.getName(who) 
