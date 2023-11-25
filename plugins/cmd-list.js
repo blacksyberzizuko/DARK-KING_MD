440e89879221fabbf6b3cd2134c084190da8bd18
Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>
2023-11-25 18:42:11 +0530

    Update cmd-list.js
    Signed-off-by: Queen-Sakura <137912767+blacksyberzizuko@users.noreply.github.com>

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -2,15 +2,15 @@
 
 let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
-*LISTA DE  COMANDOS*
+*LISTA OF COMAND*
 
-▢ *Info:* Si esta en *negrita*  esta bloqueado
-
+*Info:* If it is in *bold* it is blocked
 ──────────────────
 ${Object.entries(global.db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(bloqueado) ${key}` : key} : ${value.text}`).join('\n')}
 
 `.trim(), null, {
         mentions: Object.values(global.db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
+        m.react("📜")
     })
 }
 
